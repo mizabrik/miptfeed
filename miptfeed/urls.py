3a6173410fc212ef677e10dd8b1eee22e211cf36
Teodor Moiseev <femoiseev@gmail.com>
2016-11-20 13:26:09 +0300

    fix design 2

--- miptfeed/urls.py
+++ miptfeed/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('events.urls', namespace='events')),
     url(r'^users/', include('users.urls', namespace='users')),
+    url(r'^accounts/', include('registration.backends.simple.urls'))
 ]
